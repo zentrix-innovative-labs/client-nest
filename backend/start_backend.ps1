54c777d463c44a54a037a51846b2d9ed44620d03
eddy624 <ssebuedwin624@gmail.com>
2025-07-14 19:20:19 +0300

    modified:   backend/start_backend.ps1

--- backend/start_backend.ps1
+++ backend/start_backend.ps1
@@ -2,5 +2,6 @@
 $scriptDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
 Set-Location $scriptDir
 Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
+$env:PYTHONPATH = "$scriptDir;$(Resolve-Path $scriptDir\\..)"
 .\venv\Scripts\Activate.ps1
 waitress-serve --port=8000 config.wsgi:application 
