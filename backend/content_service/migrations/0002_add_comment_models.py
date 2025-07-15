b966bfae6dc0f583d343035dc5dce3eeaa3ec8d0
eddy624 <ssebuedwin624@gmail.com>
2025-07-15 12:30:12 +0300

    modified:   backend/content_service/migrations/0002_add_comment_models.py

--- backend/content_service/migrations/0002_add_comment_models.py
+++ backend/content_service/migrations/0002_add_comment_models.py
@@ -9,7 +9,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('content', '0001_initial'),
+        ('content_service', '0001_initial'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
