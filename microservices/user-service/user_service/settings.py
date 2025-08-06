5243cd1122b0d1bf9e4423c405b858dc16997179
Mark Cole MUKISA <markcole683@gmail.com>
2025-08-07 01:07:40 +0300

    test-checkup 4 user-service

--- microservices/user-service/user_service/settings.py
+++ microservices/user-service/user_service/settings.py
@@ -93,11 +93,11 @@ else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql',
-            'NAME': config('POSTGRES_DB_NAME'),
-            'USER': config('POSTGRES_DB_USER'),
-            'PASSWORD': config('POSTGRES_DB_PASSWORD'),
-            'HOST': config('POSTGRES_DB_HOST'),
-            'PORT': config('POSTGRES_DB_PORT'),
+            'NAME': config('POSTGRES_DB_NAME', default='client-nest'),
+            'USER': config('POSTGRES_DB_USER', default='postgres'),
+            'PASSWORD': config('POSTGRES_DB_PASSWORD', default='postgres'),
+            'HOST': config('POSTGRES_DB_HOST', default='localhost'),
+            'PORT': config('POSTGRES_DB_PORT', default='5432'),
         }
     }
 
